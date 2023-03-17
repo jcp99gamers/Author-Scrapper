d5e83443fb489d4278a2d2aa4b5a4c64f5554c45
jcp99gamers <jcp99gamer@gmail.com>
2023-03-17 14:58:39 +0530

    Taking into Account the 2nd Page, https://asianatimes.com/author/prisha-chawla/page/2/ ...

--- output/main.py
+++ output/main.py
@@ -31,46 +31,47 @@ f = open(path+filename, "w")
 f.write("# Author-Prisha-Chawla"+"\n")
 
 #
-URL = r'https://asianatimes.com/author/prisha-chawla/'
-# Make a request to the webpage
-response = requests.get(URL)
-# Parse the HTML content using Beautiful Soup
-soup = BeautifulSoup(response.content, 'html.parser')
-# Find all the article containers on the page
-article_containers = soup.find_all('article', {'class': 'l-post grid-post grid-base-post'})
-# Loop through each article container and extract the desired data
-for article in article_containers:
-    # Extract the title and link
-    title_element = article.find('a', {'class': 'image-link'})
-    title = title_element.get('title')
-    read_more_link = title_element.get('href')
-    # Extract the category and date
-    category_element = article.find('a', {'class': 'category'})
-    category = category_element.text.strip()
-    date_element = article.find('time', {'class': 'post-date'})
-    datez = date_element.get('datetime')
-    date = timeConverter(datez)
-    # Extract the excerpt
-    excerpt_element = article.find('div', {'class': 'excerpt'})
-    excerpt = excerpt_element.text.strip()
-    # Find the image source of the article
-    image_src = article.find('span', {'class': 'img'})['data-bgsrc']
-    # Print the extracted information
-    f = open(path+filename, "a")
-    f.write("##\t"+title+"\n")
-    print('Title:', title)
-    f.write("######\t"+category+"\n")
-    print('Category:', category)
-    f.write("######\t"+date+"\n")
-    print('Date:', date)
-    f.write("!["+title+"]("+image_src+")\n")
-    print('Image Source:', image_src)
-    f.write("###\t"+excerpt+"\n")
-    print('Content/Excerpt:', excerpt)
-    f.write("[Read More Link]("+read_more_link+")\n")
-    print('Read More Link:', read_more_link)
-    f.write("\n\n")
-    print()
+url = [r'https://asianatimes.com/author/prisha-chawla/',r'https://asianatimes.com/author/prisha-chawla/page/2/']
+for URL in url:
+    # Make a request to the webpage
+    response = requests.get(URL)
+    # Parse the HTML content using Beautiful Soup
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Find all the article containers on the page
+    article_containers = soup.find_all('article', {'class': 'l-post grid-post grid-base-post'})
+    # Loop through each article container and extract the desired data
+    for article in article_containers:
+        # Extract the title and link
+        title_element = article.find('a', {'class': 'image-link'})
+        title = title_element.get('title')
+        read_more_link = title_element.get('href')
+        # Extract the category and date
+        category_element = article.find('a', {'class': 'category'})
+        category = category_element.text.strip()
+        date_element = article.find('time', {'class': 'post-date'})
+        datez = date_element.get('datetime')
+        date = timeConverter(datez)
+        # Extract the excerpt
+        excerpt_element = article.find('div', {'class': 'excerpt'})
+        excerpt = excerpt_element.text.strip()
+        # Find the image source of the article
+        image_src = article.find('span', {'class': 'img'})['data-bgsrc']
+        # Print the extracted information
+        f = open(path+filename, "a")
+        f.write("##\t"+title+"\n")
+        print('Title:', title)
+        f.write("######\t"+category+"\n")
+        print('Category:', category)
+        f.write("######\t"+date+"\n")
+        print('Date:', date)
+        f.write("!["+title+"]("+image_src+")\n")
+        print('Image Source:', image_src)
+        f.write("###\t"+excerpt+"\n")
+        print('Content/Excerpt:', excerpt)
+        f.write("[Read More Link]("+read_more_link+")\n")
+        print('Read More Link:', read_more_link)
+        f.write("\n\n")
+        print()
 
 #HAD TO MAKE A DIFFERENT METHOD CAUSE THIS WEBPAGE ISNT CODED PROPERLY
 URL = r"https://www.inpactimes.com/author/prisha-chawla/"# URL of the page to be scraped
